7eb3dd418f72d181c809917c9ce69c9590393be1
superrabbit11223344 <gongwei808@hotmail.com>
2021-05-20 21:24:25 +0800

    2021-05-20 Event System

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,6 +6,10 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Hazel::Log::Init();
+	Hazel::Log::GetCoreLogger()->warn("Initialized log!");
+	Hazel::Log::GetClientLogger()->info("Hello!");
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
---
+++ Hazel/src/Hazel/Events/KeyEvent.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include "Event.h"
+
+#include <sstream>
+
+namespace Hazel {
+
+	class HAZEL_API KeyEvent : public Event
+	{
+	public:
+		inline int GetKeyCode() const { return m_KeyCode; }
+		EVENT_CLASS_CATEGORY(EventCategoryKeyboard | EventCategoryInput);
+	protected:
+		KeyEvent(int Keycode)
+			: m_KeyCode(Keycode) {}
+
+		int m_KeyCode;
+	};
+
+	class HAZEL_API KeyPressedEvent : public KeyEvent
+	{
+	public:
+		KeyPressedEvent(int keycode, int repeatCount)
+			: KeyEvent(keycode), m_RepeatCount(repeatCount) {}
+
+		inline int GetRepeatCount() const { return m_RepeatCount; }
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyPressedEvent: " << m_KeyCode << "(" << m_RepeatCount << " repeats)";
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyPressed);
+	private:
+		uint16_t m_RepeatCount;
+	};
+
+	class KeyReleasedEvent : public KeyEvent
+	{
+	public:
+		KeyReleasedEvent(const int keycode)
+			: KeyEvent(keycode) {}
+		
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyReleasedEvent: " << m_KeyCode;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyReleased);
+	};
+
+	class KeyTypedEvent : public KeyEvent
+	{
+	public:
+		KeyTypedEvent(const int keycode)
+			: KeyEvent(keycode) {}
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyTypedEvent: " << m_KeyCode;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyTyped);
+}
